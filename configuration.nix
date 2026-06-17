96c2330c7f914ea48a291d1d3a1dd658e1a723a3
Alberth Matos <alberth@matos.cc>
2026-06-17 01:26:14 -0400

    chore: Enable flakes

--- configuration.nix
+++ configuration.nix
@@ -71,6 +71,9 @@
     ];
   };
 
+  # Enable the Flakes feature and the accompanying new nix command-line tool
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # programs.firefox.enable = true;
 
   # List packages installed in system profile.
